c8d84d93ffe0a3f33e8148cb3b43cff62ffb08a0
naneraruom <naneraruom@gmail.com>
2020-11-23 20:35:58 -0300

    iniciando layout

---
+++ jokenpo/lib/tela_jokenpo.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class TelaJokenpo extends StatefulWidget {
+  @override
+  _TelaJokenpoState createState() => _TelaJokenpoState();
+}
+
+class _TelaJokenpoState extends State<TelaJokenpo> {
+
+  Widget option(){
+    return Row(
+      children: <Widget>[
+
+      ],
+    );
+  }
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+
+      body: Column(
+        children: <Widget>[
+
+        ]
+      ),
+      
+    );
+  }
+}
