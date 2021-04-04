6f88cd0226c3754d24e9cbacbd2902a0a7a979c8
renanmoura6941 <naneraruom@gmail.com>
2021-04-04 09:36:33 -0300

    sharedPrefrences

--- shared_data_manipulation/lib/HomeScreen.dart
+++ shared_data_manipulation/lib/HomeScreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -6,30 +7,51 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  String text = "vazio";
+  TextEditingController _controller = TextEditingController();
+  SharedPreferences prefs;
 
-  _save(){
-
+  _save() async {
+    prefs = await SharedPreferences.getInstance();
+    await prefs.setString("name", _controller.text);
+    print("salvo ${prefs.getString("name")}");
   }
 
-  _recover(){
+  _recover() async {
+    prefs = await SharedPreferences.getInstance();
+    setState(() {
+      text = prefs.getString("name")??"sem valor";
+    });
+  }
 
+  _delete() async {
+    prefs = await SharedPreferences.getInstance();
+    setState(() {
+      prefs.remove("name");
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: Column(
-          children: [
-            Text(""),
-            TextField(),
-            Row(
-              children: [
-                ElevatedButton(onPressed: () {}, child: Text("save")),
-                ElevatedButton(onPressed: () {}, child: Text("recover"))
-              ],
-            )
-          ],
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              Text(text ?? "vazio"),
+              TextField(
+                controller: _controller,
+              ),
+              Row(
+                children: [
+                  ElevatedButton(onPressed: _save, child: Text("save")),
+                  ElevatedButton(onPressed: _recover, child: Text("recover")),
+                  ElevatedButton(onPressed: _delete, child: Text("delete"))
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
